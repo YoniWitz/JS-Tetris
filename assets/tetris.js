feefb39e05b27067a2b669492b7a54ef4ccd87dc
Yoni Hirshowitz <jonhirsh39@gmail.com>
2019-12-18 21:38:12 -0500

    got tetrominos to pile up on bottom line

--- assets/tetris.js
+++ assets/tetris.js
@@ -30,33 +30,42 @@ drawBorderLines = () => {
 
 draw = (background) => {
     if (typeof background.currentTetromino.lowestRLocation === 'undefined' || background.currentTetromino.lowestRLocation >= BOXES_ROW_COUNT - 1) {
-        let tempTetromino;
         let rand;
-        rand = Math.round(Math.random() * (NUMBER_OF_TETROMINOS - 1));
 
-        background.currentTetromino = background.getRandomTetromino(rand);
-
-        for (let r = background.currentTetromino.tetrominoMatrix.length - 1; r >= 0; r--) {
-            tempTetromino = JSON.parse(JSON.stringify(background.currentTetromino.tetrominoMatrix[r]));
-            background.currentTetromino.tetrominoMirror.unshift(tempTetromino)
-            background.backgroundMatrix.unshift(tempTetromino);
+        //when tetromino hits bottom, tetromino is added to backgroundMatrix and magic happens
+        //TO-DO
+        if (background.currentTetromino.lowestRLocation >= BOXES_ROW_COUNT - 1) {
+            for (let r = background.currentTetromino.lowestRLocation - 3; r < background.backgroundMatrix.length; r++) {
+                for (let c = 0; c < background.currentTetromino.tetrominoLength; c++)
+                    background.backgroundMatrix[r][c].status |= background.currentTetromino.tetrominoMatrix[r][c].status;
+            }
         }
+        ctx.clearRect(PADDING * 2, PADDING * 2, GAME_WIDTH - PADDING * 3, CANVAS_HEIGHT - 3 * PADDING);
+        background.drawBoxes(background.backgroundMatrix);
+
+        rand = Math.round(Math.random() * (NUMBER_OF_TETROMINOS - 1));
+        if (DEBUG) {
+            rand = 0;
+        }
+        background.currentTetromino = background.getRandomTetromino(rand);
     }
     else {
         //if no key was pressed
         if (!background.currentTetromino.keydownFlag) {
-            background.backgroundMatrix.unshift(JSON.parse(JSON.stringify(background.newRow)));
-            background.backgroundMatrix.pop();
+            //add new row to 
+            background.currentTetromino.tetrominoMatrix.unshift(JSON.parse(JSON.stringify(background.newRow)));
+
             background.currentTetromino.lowestRLocation++;
+
         }
         else {
             background.currentTetromino.keydownFlag = false;
         }
+        background.addCoordinates(background.currentTetromino.tetrominoMatrix);
+        background.drawBoxes(background.currentTetromino.tetrominoMatrix);
     }
 
-    background.addCoordinates();
-    ctx.clearRect(PADDING * 2, PADDING * 2, GAME_WIDTH - PADDING * 3, CANVAS_HEIGHT - 3 * PADDING);
-    background.drawBoxes();
+
     // requestAnimationFrame(draw);
 }
 
@@ -139,6 +148,7 @@ window.onload = () => {
             for (let c = 0; c < BOXES_COLUMN_COUNT; c++) {
                 this.newRow[c] = { status: 0 };
             }
+
         },
         createInitialbackgroundMatrix: function () {
             for (let r = 0; r < BOXES_ROW_COUNT; r++) {
@@ -147,11 +157,12 @@ window.onload = () => {
                     this.backgroundMatrix[r][c] = { status: 0 };
                 }
             }
+            this.addCoordinates(this.backgroundMatrix);
         },
-        drawBoxes: function () {
-            for (let r = 0; r < BOXES_ROW_COUNT; r++) {
-                for (let c = 0; c < BOXES_COLUMN_COUNT; c++) {
-                    if (this.backgroundMatrix[r][c].status === 0) {
+        drawBoxes: function (matrix) {
+            for (let r = 0; r < matrix.length; r++) {
+                for (let c = 0; c < matrix[r].length; c++) {
+                    if (matrix[r][c].status === 0) {
                         color = "gray"
                     }
                     else {
@@ -160,35 +171,33 @@ window.onload = () => {
 
                     ctx.beginPath();
                     //outer box
-                    ctx.strokeRect(this.backgroundMatrix[r][c].outerX, this.backgroundMatrix[r][c].outerY, BOXES_BORDER_WIDTH, BOXES_BORDER_HEIGHT);
+                    ctx.strokeRect(matrix[r][c].outerX, matrix[r][c].outerY, BOXES_BORDER_WIDTH, BOXES_BORDER_HEIGHT);
                     //inner box
-                    ctx.rect(this.backgroundMatrix[r][c].innerX, this.backgroundMatrix[r][c].innerY, BOXES_INNER_BOX_WIDTH, BOXES_INNER_BOX_HEIGHT);
+                    ctx.rect(matrix[r][c].innerX, matrix[r][c].innerY, BOXES_INNER_BOX_WIDTH, BOXES_INNER_BOX_HEIGHT);
                     ctx.fillStyle = color;
                     ctx.fill();
                     ctx.closePath();
                 }
             }
         },
-        addCoordinates: function () {
-            for (let r = 0; r < this.backgroundMatrix.length; r++) {
-                for (let c = 0; c < this.backgroundMatrix[r].length; c++) {
+        addCoordinates: function (matrix) {
+            for (let r = 0; r < matrix.length; r++) {
+                for (let c = 0; c < matrix[r].length; c++) {
                     let outerBoxX = PADDING * 2 + BOXES_PADDING * (1 + 2 * c) + (c * BOXES_BORDER_WIDTH);
                     let outerBoxY = PADDING * 2 + BOXES_PADDING * (1 + 2 * r) + (r * BOXES_BORDER_HEIGHT);
 
                     let innerBoxX = PADDING * 2 + INNER_TO_OUTER_RATIO * BOXES_PADDING * (1 + 2 * c) + (c * BOXES_INNER_BOX_WIDTH);
                     let innerBoxY = PADDING * 2 + INNER_TO_OUTER_RATIO * BOXES_PADDING * (1 + 2 * r) + (r * BOXES_INNER_BOX_HEIGHT);
 
-                    this.backgroundMatrix[r][c].outerX = outerBoxX;
-                    this.backgroundMatrix[r][c].outerY = outerBoxY;
+                    matrix[r][c].outerX = outerBoxX;
+                    matrix[r][c].outerY = outerBoxY;
 
-                    this.backgroundMatrix[r][c].innerX = innerBoxX;
-                    this.backgroundMatrix[r][c].innerY = innerBoxY;
+                    matrix[r][c].innerX = innerBoxX;
+                    matrix[r][c].innerY = innerBoxY;
                 }
             }
         }
     };
-
-
 
     background.createInitialbackgroundMatrix();
     background.createNewRow();
@@ -209,7 +218,6 @@ class Tetromino {
     farRightCLocation;
     rotationCLocation;
     rotationState;
-    tetrominoMirror;
 
     tetrominoLength;
 
@@ -222,7 +230,7 @@ class Tetromino {
 
             this.farRightCLocation++; this.farLeftCLocation++; this.rotationCLocation++;
 
-            this.tetrominoMirror.forEach(item => {
+            this.tetrominoMatrix.forEach(item => {
                 item.unshift(item.pop());
             })
         }
@@ -236,7 +244,7 @@ class Tetromino {
 
             this.farRightCLocation--; this.farLeftCLocation--; this.rotationCLocation--;
 
-            this.tetrominoMirror.forEach(item => {
+            this.tetrominoMatrix.forEach(item => {
                 item.push(item.shift());
             })
         }
@@ -249,7 +257,7 @@ class Tetromino {
             if (this.rotationState > 1) {
                 this.rotationState = 0;
             }
-            //switching from horizontal to vertical
+            //switching from vertical to horizontal
             if (this.rotationState === 1) {
                 //if iTetromino against left wall or one away from left wall or against right wall, cant rotate
                 if (this.farLeftCLocation < 2 || this.farRightCLocation === this.tetrominoLength - 1) {
@@ -258,24 +266,23 @@ class Tetromino {
                     this.rotationState++;
                     this.keydownFlag = true;
 
-                    this.lowestRLocation -=2;
+                    this.lowestRLocation -= 2;
                     this.farLeftCLocation = this.rotationCLocation - 2;
                     this.farRightCLocation = this.rotationCLocation + 1;
 
-                    //erase horizontal
-                    for (let r = 0; r < this.tetrominoMatrix.tetrominoLength; r++) {
-                        this.tetrominoMirror[r][this.rotationCLocation].status = 0;
+                    //erase vertical
+                    for (let r = this.lowestRLocation - 1; r < this.tetrominoMatrix.length; r++) {
+                        this.tetrominoMatrix[r][this.rotationCLocation].status = 0;
                     }
 
-                    //create vertical
-                    this.tetrominoMirror[1][this.rotationCLocation - 1].status = 1;
-                    this.tetrominoMirror[1][this.rotationCLocation].status = 1;
-                    this.tetrominoMirror[1][this.rotationCLocation - 2].status = 1;
-                    this.tetrominoMirror[1][this.rotationCLocation + 1].status = 1;
+                    //create horizontal
+                    this.tetrominoMatrix[this.lowestRLocation][this.rotationCLocation - 1].status = 1;
+                    this.tetrominoMatrix[this.lowestRLocation][this.rotationCLocation].status = 1;
+                    this.tetrominoMatrix[this.lowestRLocation][this.rotationCLocation - 2].status = 1;
+                    this.tetrominoMatrix[this.lowestRLocation][this.rotationCLocation + 1].status = 1;
                 }
             }
-            //switching from vertical to horizontal
-
+            //switching from horizontal to vertical
             else {
                 //if iTetromino on bottom 2 rows row or if tetromino on top row
                 if (this.lowestRLocation >= BOXES_ROW_COUNT - 2 || this.lowestRLocation === 0) {
@@ -285,17 +292,17 @@ class Tetromino {
                     this.rotationState++;
                     this.keydownFlag = true;
 
-                    this.lowestRLocation +=2;
+                    this.lowestRLocation += 2;
                     this.farLeftCLocation = this.farRightCLocation = this.rotationCLocation;
 
                     //erase horizontal
                     for (let c = 0; c < this.tetrominoLength; c++) {
-                        this.tetrominoMirror[1][c].status = 0;
+                        this.tetrominoMatrix[this.lowestRLocation - 2][c].status = 0;
                     }
 
                     //create vertical
-                    for (let r = 0; r < this.tetrominoMatrix.tetrominoLength; r++) {
-                        this.tetrominoMirror[r][this.rotationCLocation].status = 1;
+                    for (let r = this.lowestRLocation - 3; r < this.tetrominoMatrix.length; r++) {
+                        this.tetrominoMatrix[r][this.rotationCLocation].status = 1;
                     }
                 }
             }
@@ -657,8 +664,7 @@ class Tetromino {
         this.letter = letter;
         this.tetrominoMatrix = tetrominoMatrix;
         this.rotationState = 0;
-        this.tetrominoMirror = [];
-        this.lowestRLocation = this.letter === 'i'? 1:2;
+        this.lowestRLocation = this.letter === 'i' ? 1 : 2;
         this.tetrominoLength = 10;
     }
 }
